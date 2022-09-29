902cb5cfbb60e64a2fb58dcfdc18c00c4ff90226
Tristiu <ileana.tristiu@agilenetworks.tech>
2022-09-29 15:23:11 +0300

    show also Courses

--- CodeFirstApp/CodeFirstApp/Program.cs
+++ CodeFirstApp/CodeFirstApp/Program.cs
@@ -29,7 +29,7 @@ namespace CodeFirstApp
             
 
             context.DisplayStudents();
-            //context.DisplayCourses();
+            context.DisplayCourses();
 
             Console.ReadLine();
         }
